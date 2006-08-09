fa62099fc61cb9e73f14b6da03f6b50c145c3a57
thomasdesvenain <none>
2006-08-09 14:20:37 +0200

    record changes to migrate the site
    darcs-hash:20060809122037-3fa84-de59a0cfc2e139e7261e430a60b0d8d0d98aabe9.gz

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ import sys
 
 from Products.CMFCore.DirectoryView import registerDirectory
 from Products.CMFCore import utils
+
 from setup import BDRSitePolicy
 
 this_module = sys.modules[__name__]
